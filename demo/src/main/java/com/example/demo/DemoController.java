39905659dd4e3fe7b03b2667adc841f6936b04c2
ayaneppi <fujiwara_a01@pci-sol.com>
2022-01-12 13:38:37 +0900

    java

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 //import省略
@@ -13,11 +18,27 @@ public class DemoController {
 	@Autowired Repository cr;
 
 	@RequestMapping("/")
-	public ModelAndView index() {
+	public String index() {
+      return "index";
+	}
+	@RequestMapping("/heroku")
+	public ModelAndView heroku(
+		@RequestParam("Id")Long Id,
+		@RequestParam("newName")String newName,
+		Model model
+		) {
 		ModelAndView mav = new ModelAndView();
-
-		Iterable<Property> customerList = cr.findAll();
-		mav.addObject("customerList", customerList);
+		Iterable<Property> findPropertyList = cr.findAll();
+		List<Object> propertyList = new ArrayList<Object>();
+		for(Property p : findPropertyList){
+			//入力されたIdと一致した場合は変更前のProperty名を取得する
+			if(p.getId() == Id) {
+				propertyList.add(Id);
+				propertyList.add(p.getName());
+				propertyList.add(newName);
+			}
+		}
+		mav.addObject("propertyList", propertyList);
 		mav.setViewName("temp");
       return mav;
 	}
